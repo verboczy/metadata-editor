868e38cfebe250394992e3cd5d1e4f71c7faeae5
Verbőczy <kristof.verboczy@gmail.com>
2021-01-27 19:22:25 +0100

    FXML init

---
+++ src/main/java/application/Main.java
@@ -0,0 +1,27 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/browser.fxml"));
+        primaryStage.initStyle(StageStyle.DECORATED);
+        primaryStage.setTitle("Login Page");
+        primaryStage.setScene(new Scene(root, 520, 400));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
