6381ab3637a6bb308f1a2d37310149b551f69e38
Verbőczy <kristof.verboczy@gmail.com>
2021-03-14 22:54:12 +0100

    Metadata predicate

---
+++ src/main/java/predicate/MetadataPredicate.java
@@ -0,0 +1,36 @@
+package predicate;
+
+import metadata.Metadata;
+import metadata.MetadataReader;
+import metadata.MetadataSearch;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class MetadataPredicate {
+    private final boolean isEnabled;
+    private final List<MetadataSearch> metadataList;
+    private final MetadataReader metadataReader;
+
+    public MetadataPredicate(boolean isEnabled, List<MetadataSearch> metadataList) {
+        this.isEnabled = isEnabled;
+        this.metadataList = metadataList;
+        this.metadataReader = new MetadataReader();
+    }
+
+    public Predicate<File> getPredicate() {
+        return file -> {
+            if (!isEnabled || metadataList.isEmpty()) {
+                return true;
+            }
+
+            final Path path = Paths.get(file.getPath());
+            final List<Metadata> fileMetadataList = metadataReader.read(path);
+
+            return metadataList.stream().allMatch(metadata -> fileMetadataList.stream().anyMatch(metadata::matchesMetadata));
+        };
+    }
+}
